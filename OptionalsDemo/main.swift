40a413757f7d9221d70c7a860d7abd05cedcd105
keoki33 <gwong323@yahoo.com>
2020-03-30 15:43:50 +1100

    optionals lesson done

--- OptionalsDemo/main.swift
+++ OptionalsDemo/main.swift
@@ -1,12 +1,62 @@
-//
-//  main.swift
-//  OptionalsDemo
-//
-//  Created by Keoki on 30/3/20.
-//  Copyright © 2020 Keoki. All rights reserved.
-//
 
-import Foundation
 
-print("Hello, World!")
+
+var myOptional: String?
+
+myOptional = nil
+
+if myOptional != nil {
+    let text: String  = myOptional!
+} else {
+    print("myOptional is nil")
+}
+
+myOptional = "Yo"
+
+//Example of Optional Binding - binds optional to new constant
+
+if let x = myOptional {
+    let text: String = x
+    print(x)
+} else
+{
+    print("myOptional is nil")
+}
+
+
+//Example of Nil coalescing operator - allows default value if value is nil
+
+myOptional = nil
+
+let text: String = myOptional ?? "default value"
+
+print(text)
+
+
+/// Example of optional chaining ///////
+
+// if nil will not continue
+//
+//  optional?.property
+//  optional?.method
+
+struct Test {
+    var x = 123
+    
+    func testFunc()  {
+        print("test")
+    }
+    
+}
+
+let test: Test?
+
+test = Test()
+
+print(test?.x)
+
+test?.testFunc()
+
+
+
 
